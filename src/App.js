4611edeefb2c2f35a918a36602192f65de67b1f6
arybyt <bayatiarya@gmail.com>
2022-11-04 17:37:05 -0500

    props and collection card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={'BANDANA PUNK'}
+        traits={[{ value: 7 }]}
+        image={
+          'https://i.seadn.io/gae/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q?auto=format&w=1000'
+        }
+      />
     </div>
   );
 }
